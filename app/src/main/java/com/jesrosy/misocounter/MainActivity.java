8571af67c8b585cd50725da41f7cc42205701931
EunSeok Jeon <jes901110@gmail.com>
2018-07-11 02:58:28 +0900

    Tutorial finish

--- app/src/main/java/com/jesrosy/misocounter/MainActivity.java
+++ app/src/main/java/com/jesrosy/misocounter/MainActivity.java
@@ -1,7 +1,10 @@
 package com.jesrosy.misocounter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void onButton1Clicked(View v){
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(myIntent);
+    }
+
+    public void onButton2Clicked(View v){
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1000-1000"));
+        startActivity(myIntent);
+    }
+    public void onButton3Clicked(View v){
+        Intent myIntent = new Intent(getApplicationContext(), MenuActivity.class);
+        startActivity(myIntent);
     }
 }
